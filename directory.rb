5259bd6d37ab4c1896017ad38e4d2a84adf0b71f
Alex <alexrtucker@hotmail.co.uk>
2019-11-27 15:31:23 +0000

    Improved directory file

--- directory.rb
+++ directory.rb
@@ -1,19 +1,22 @@
+#add students to an array
+students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
 #print list of students
 puts "The students of Villians Academy"
 puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+students.each do |student|
+  puts student
+end
 #print number of students
-print "Overall, we have "
-#print does not add new line characters
-print 11
-puts " great students."
+puts "Overall, we have #{students.count} great students."
