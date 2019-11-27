d3b111906f4af476ac615b880b870f744f80337d
Alex <alexrtucker@hotmail.co.uk>
2019-11-27 15:41:58 +0000

    Sectioned into methods

--- directory.rb
+++ directory.rb
@@ -13,10 +13,23 @@ students = [
   "Norman Bates"
 ]
 #print list of students
-puts "The students of Villians Academy"
-puts "-------------"
-students.each do |student|
-  puts student
+def print_header
+  puts "The students of Villians Academy"
+  puts "-------------"
 end
-#print number of students
-puts "Overall, we have #{students.count} great students."
+
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+
+def print_footer(names)
+  #print number of students
+  puts "Overall, we have #{names.count} great students."
+end
+
+#nothing will happen unless the metods are called.
+print_header
+print(students)
+print_footer(students)
